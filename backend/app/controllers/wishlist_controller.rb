92caac2d2aa4f80a7afbd04ceaddd0c3e20c3909
akrueger12 <anna.r.krueger1@gmail.com>
2021-08-07 11:33:47 -0500

    first working controller!

--- backend/app/controllers/wishlist_controller.rb
+++ backend/app/controllers/wishlist_controller.rb
@@ -1,5 +1,22 @@
 class WishlistController < ApplicationController
   def create
+    puts(params[:wishlist_id])
+    puts(params[:user_input])
+    @wishlistItem = Wishlist.new(
+      wishlist_id: params[:wishlist_id],
+      user_input: params[:user_input]
+    )
+
+    if !@wishlistItem.save
+      respond_to do |format|
+        format.json { render json: { error: :unprocessable_entity }, status: :unprocessable_entity }
+      end
+      return
+    end
+
+    respond_to do |format|
+      format.json { render json: { id: @wishlistItem[:id] } }
+    end
   end
 
   def show
