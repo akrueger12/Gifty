964c47b6e1f10b4c6e04596fad30e5c30b85581e
akrueger12 <anna.r.krueger1@gmail.com>
2021-08-07 15:54:29 -0500

    gift finding algorithm works!

--- backend/app/controllers/gifts_controller.rb
+++ backend/app/controllers/gifts_controller.rb
@@ -21,6 +21,53 @@ class GiftsController < ApplicationController
   end
 
   def show
+    #get the ids from the string keywords
+    @keyWordArray = params[:keywords]
+    @keyWordIDs = Array.new()
+    @keyWordArray.each { |key|
+      @key = Keyword.find_by(keyword: key)
+      @keyWordIDs.push(@key.id)
+  }
+
+    @giftIDsArray = Array.new()
+
+    #get all the matching gifts that have those keywords 
+    #adding duplicates to the array is fine!
+    @keyWordIDs.each { |keyID| 
+      @giftIDs = GiftToKeyword.where(keyword_id: keyID).collect(&:gift_id)
+      puts("PUT HERE")
+      puts(@giftIDs)
+      @giftIDs.each { |id|
+        @giftIDsArray.push(id)
+      }
+    } 
     
+    puts("yo2")
+
+    #sort them by the gift id
+    @matchingKeywordCount = Hash.new(0)
+    @giftIDsArray.each { |giftID| @matchingKeywordCount[giftID] +=1}
+    @matchingKeywordCount.sort_by { |giftID, number| number}.last[0]
+    @topGiftIDs = @matchingKeywordCount.keys
+
+    puts("yo3")
+
+    #return the gifts that are most common in the array
+    @top6Gifts = Array.new()
+    if @topGiftIDs.length >= 6
+      for i in 0..5 do
+        @topGift = Gift.find(@topGiftIDs[i])
+        @top6Gifts.push(@topGift)
+      end
+    else
+      for i in 0..(@topGiftIDs.length -1) do
+        @topGift = Gift.find(@topGiftIDs[i])
+        @top6Gifts.push(@topGift)
+      end
+    end
+
+    respond_to do |format|
+      format.json { render json: { gifts: @top6Gifts } }
+    end
   end
 end
