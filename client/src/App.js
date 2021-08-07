6f3a111bd3f2be29370f9fa9e70d61bbdd308ce7
EmlyDltn <54635915+EmlyDltn@users.noreply.github.com>
2021-08-07 14:39:00 -0500

    Start on login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import GiftForm from './GiftForm';
+import Login from './Login'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <GiftForm/>
       </header>
+      <Login/>
     </div>
   );
 }
