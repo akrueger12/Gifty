498c0b38fbfae3497f877227bb0ffb32a7ea35d6
Vivian Jacobitz <jacobitz.vivian@cox.net>
2021-08-07 22:35:12 -0500

    all the apis possibly

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -44,3 +44,69 @@ export const loginUser = async (username, password) => {
         }
     );
 }
+
+export const retrieveWishList = async (userId) => {
+    let url = `${BASE_URL}/wishlist/show`;
+
+    const data = { user_id: userId };
+
+    await fetch(url, {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return new Error('there was an error');
+            }
+        }
+    );
+}
+
+export const retrieveGiftsForUser = async (keywords) => {
+    let url = `${BASE_URL}/gifts/show`;
+
+    const data = { keywords: keywords };
+
+    await fetch(url, {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return new Error('there was an error');
+            }
+        }
+    );
+}
+
+export const createNewItem = async (userId, item) => {
+    let url = `${BASE_URL}/wishlist/create`;
+
+    const data = { wishlist_id: userId, user_input: item };
+
+    await fetch(url, {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                return new Error('there was an error');
+            }
+        }
+    );
+}
