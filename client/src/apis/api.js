519db4d122c87aa2dc3709703ba0232a22b19222
akrueger12 <anna.r.krueger1@gmail.com>
2021-08-20 16:41:59 -0500

    api url

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,5 +1,5 @@
-//const BASE_URL = 'https://gifty-web-app.herokuapp.com/';
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'https://gifty-web-app.herokuapp.com/';
+//const BASE_URL = 'http://localhost:3000/';
 
 export const createNewUser = async (username, password) => {
     let url = `${BASE_URL}/users/create`;
